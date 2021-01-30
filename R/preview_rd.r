eee7cb39d5fe82ddafeae66b57b6a5b242a07598
Wolfgang Viechtbauer <wvb@wvbauer.com>
2021-01-30 16:10:02 +0000

    version 1.2-0

--- R/preview_rd.r
+++ R/preview_rd.r
@@ -1,4 +1,4 @@
-preview_rd <- function(Rdfile, view = TRUE, type = "html", verbose = FALSE) {
+preview_rd <- function(Rdfile, view = TRUE, type = "html", verbose = FALSE, dark = FALSE) {
 
    if (missing(Rdfile))
       stop("Need to specify 'Rdfile' argument.")
@@ -41,9 +41,9 @@ preview_rd <- function(Rdfile, view = TRUE, type = "html", verbose = FALSE) {
    if (verbose)
       message("Found Rd file: ", Rdfile.loc)
 
-   # get environment with the mathjaxr and system macros
-   # (might also want to check if package itself has/loads macros and merge these in as well)
+   # get environment with the mathjaxr, package, and system macros
    macros <- tools::loadPkgRdMacros(system.file(package = "mathjaxr"))
+   macros <- tools::loadPkgRdMacros(ifelse(basename(getwd()) == "man", "..", "."), macros = macros)
    macros <- tools::loadRdMacros(file.path(R.home("share"), "Rd", "macros", "system.Rd"), macros = macros)
 
    # generate name of temp file
@@ -54,10 +54,16 @@ preview_rd <- function(Rdfile, view = TRUE, type = "html", verbose = FALSE) {
 
    if (type == "html") {
 
+      if (dark) {
+         css <- system.file("doc/R_dark.css", package = "mathjaxr")
+      } else {
+         css <- system.file("html/R.css", package = "mathjaxr")
+      }
+
       # convert Rd to HTML version
       html <- tools::Rd2HTML(Rdfile.loc, outfile,
          macros = macros, permissive = TRUE,
-         stylesheet = system.file("html/R.css", package = "mathjaxr"))
+         stylesheet = css)
 
       # use viewer if available (as in RStudio); otherwise use browseURL()
       viewer <- getOption("viewer")
@@ -92,8 +98,23 @@ preview_rd <- function(Rdfile, view = TRUE, type = "html", verbose = FALSE) {
 
    if (type == "pdf") {
 
+      pkg <- character(0)
+
+      if (file.exists(ifelse(basename(getwd()) == "man", "../DESCRIPTION", "DESCRIPTION"))) {
+         pkg <- suppressWarnings(try(read.dcf(ifelse(basename(getwd()) == "man", "../DESCRIPTION", "DESCRIPTION"), fields="Package"), silent=TRUE))
+         if (inherits(pkg, "try-error") || is.na(pkg[1]))
+            pkg <- character(0)
+      }
+
+      if (length(pkg) > 0L) {
+         RdMacros <- paste0("mathjaxr,", pkg)
+      } else {
+         RdMacros <- "mathjaxr"
+      }
+
       #cmd <- paste0("CMD Rd2pdf --no-index --no-description --force --batch --RdMacros=mathjaxr ", ifelse(view, "", "--no-preview"), " --output=", outfile, " ", Rdfile.loc)
-      cmd <- paste0("CMD Rd2pdf --no-index --no-description --force --batch --RdMacros=mathjaxr --no-preview --output=", outfile, " ", Rdfile.loc)
+      #cmd <- paste0("CMD Rd2pdf --no-index --no-description --force --batch --RdMacros=mathjaxr --no-preview --output=", outfile, " ", Rdfile.loc)
+      cmd <- paste0("CMD Rd2pdf --no-index --no-description --force --batch --RdMacros=", RdMacros, " --no-preview --output=", outfile, " ", Rdfile.loc)
       system2("R", cmd, wait=TRUE, stdout = ifelse(verbose >= 2, "", tempfile("stdout")))
 
       if (.Platform$OS.type == "windows") {
--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 mathjaxr: Using Mathjax in Rd Files
 ===================================
 
-[![Build Status](https://travis-ci.org/wviechtb/mathjaxr.svg?branch=master)](https://travis-ci.org/wviechtb/mathjaxr)
+[![R build status](https://github.com/wviechtb/mathjaxr/workflows/R-CMD-check/badge.svg)](https://github.com/wviechtb/mathjaxr/actions)
 [![CRAN Version](https://www.r-pkg.org/badges/version/mathjaxr)](https://CRAN.R-project.org/package=mathjaxr)
-![devel Version](https://img.shields.io/badge/devel-1.1--0-brightgreen.svg)
+![devel Version](https://img.shields.io/badge/devel-1.2--0-brightgreen.svg)
 
 ## Description
 
@@ -17,7 +17,7 @@ One can then enable the use of MathJax by calling the `\loadmathjax` macro (that
 
 An inline equation can then be added with the `\mjeqn{latex}{ascii}` macro, with the LaTeX commands for the equation given between the first set of curly brackets (which will be rendered in the HTML and PDF help pages) and the plain-text version of the equation given between the second set of curly brackets (which will be shown in the plain text help). With the `\mjdeqn{latex}{ascii}` macro, one can add 'displayed equations' (as in LaTeX's `displaymath` environment).
 
-Single argument versions of these macros, namely `\mjseqn{latexascii}` and `\mjsdeqn{latexascii}`, are also available. For the rare case that one must specify different LaTeX commands for the PDF and HTML pages, there are also triple argument versions of these macros, namely `\mjteqn{pdflatex}{htmllatex}{ascii}` and `\mjtdeqn{pdflatex}{htmllatex}{ascii}`.
+Single argument versions of these macros, `\mjseqn{latexascii}` and `\mjsdeqn{latexascii}`, are also available. For the relatively rare case that one must specify different LaTeX commands for the PDF and HTML pages, there are also triple argument versions of these macros, namely `\mjteqn{pdflatex}{htmllatex}{ascii}` and `\mjtdeqn{pdflatex}{htmllatex}{ascii}`.
 
 ## Details
 
@@ -47,17 +47,20 @@ where ![\mu](https://render.githubusercontent.com/render/math?math=%5Cmu) denote
 
 ## Installation
 
-The current official (i.e., [CRAN](https://cran.r-project.org/package=mathjaxr)) release can be installed directly within R with:
+The current official (i.e., [CRAN](https://cran.r-project.org/package=mathjaxr)) release can be installed within R with:
 
 ```r
 install.packages("mathjaxr")
 ```
 
-After installing the [remotes](https://cran.r-project.org/package=remotes) package with ```install.packages("remotes")```, the development version of the `mathjaxr` package can be installed with:
+The development version of the package can be installed with:
 
 ```r
+install.packages("remotes")
 remotes::install_github("wviechtb/mathjaxr")
 ```
+
+This builds the package from source based on the current version on [GitHub](https://github.com/wviechtb/mathjaxr).
 
 ## Meta
 
